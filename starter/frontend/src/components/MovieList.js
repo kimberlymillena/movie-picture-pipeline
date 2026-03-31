2baa50ef87b78d0b41cc2cf13f8c2e1cfd544341
Kimberly Millena <kimberly.m.millena@accenture.com>
2026-03-31 13:04:12 +0000

    Fix frontend MovieList component for CI build

---
+++ starter/frontend/src/components/MovieList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+export default function MovieList({ onMovieClick }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const baseUrl = process.env.REACT_APP_MOVIE_API_URL || 'http://localhost:5000';
+    fetch(`${baseUrl}/movies`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.movies || []))
+      .catch(() => setMovies([]));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {movies.map((m) => (
+          <li
+            key={m.id || m.title}
+            onClick={() => onMovieClick && onMovieClick(m)}
+            style={{ cursor: 'pointer' }}
+          >
+            {m.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+``
